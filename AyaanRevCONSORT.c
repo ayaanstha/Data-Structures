dc9baf93282e8cc7e2bda0637272c5a5f673953e
ayaanstha <ayaanshrestha.cs23@bmsce.ac.in>
2024-11-19 10:54:13 +0530

    Update AyaanRevCONSORT.c

--- AyaanRevCONSORT.c
+++ AyaanRevCONSORT.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the structure for a node
+
 struct Node {
     int data;
     struct Node* next;
 };
 
-// Function to create a new node
+
 struct Node* createNode(int data) {
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = data;
@@ -15,7 +15,7 @@ struct Node* createNode(int data) {
     return newNode;
 }
 
-// Function to append a node at the end of the linked list
+
 void append(struct Node** head, int data) {
     struct Node* newNode = createNode(data);
     if (*head == NULL) {
@@ -29,7 +29,7 @@ void append(struct Node** head, int data) {
     }
 }
 
-// Function to print the linked list
+
 void printList(struct Node* head) {
     struct Node* temp = head;
     while (temp != NULL) {
@@ -39,22 +39,22 @@ void printList(struct Node* head) {
     printf("\n");
 }
 
-// Function to reverse the linked list
+
 void reverse(struct Node** head) {
     struct Node* prev = NULL;
     struct Node* current = *head;
     struct Node* next = NULL;
 
     while (current != NULL) {
-        next = current->next; // Store the next node
-        current->next = prev; // Reverse the current node's pointer
-        prev = current;       // Move prev and current one step forward
+        next = current->next; 
+        current->next = prev; 
+        prev = current;       
         current = next;
     }
-    *head = prev; // Update head to the new first element
+    *head = prev; 
 }
 
-// Function to concatenate two linked lists
+
 void concatenate(struct Node** head1, struct Node* head2) {
     if (*head1 == NULL) {
         *head1 = head2;
@@ -67,7 +67,7 @@ void concatenate(struct Node** head1, struct Node* head2) {
     }
 }
 
-// Function to sort the linked list using Bubble Sort
+
 void sortList(struct Node** head) {
     if (*head == NULL || (*head)->next == NULL) {
         return;  // No need to sort if the list is empty or has only one element
@@ -89,12 +89,12 @@ void sortList(struct Node** head) {
     }
 }
 
-// Main function to demonstrate reverse, concatenate, and sort
+
 int main() {
     struct Node* list1 = NULL;
     struct Node* list2 = NULL;
 
-    // Insert elements into list1
+   
     append(&list1, 5);
     append(&list1, 3);
     append(&list1, 8);
@@ -102,24 +102,24 @@ int main() {
     printf("List 1: ");
     printList(list1);
 
-    // Insert elements into list2
+    
     append(&list2, 6);
     append(&list2, 2);
     append(&list2, 7);
     printf("List 2: ");
     printList(list2);
 
-    // Concatenate list2 to list1
+    
     concatenate(&list1, list2);
     printf("After concatenating list2 to list1: ");
     printList(list1);
 
-    // Sort list1
+    
     sortList(&list1);
     printf("After sorting list1: ");
     printList(list1);
 
-    // Reverse list1
+   
     reverse(&list1);
     printf("After reversing list1: ");
     printList(list1);
